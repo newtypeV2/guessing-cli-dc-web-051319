2bb8cb0c62f0ff7896f05a85612882a829856cd5
Gian Libunao <gianlibunao@gmail.com>
2019-05-07 19:25:05 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,10 +1,12 @@
 # Code your solution here!
 def run_guessing_game
-  
+  numToGuess = rand(100)
   while true do
     puts "User input:"
     input=gets.chomp
     case input
+    when input.to_i == 30
+      puts "You've guessed the correct number!"
     when "exit"
       break
     end
